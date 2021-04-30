f30b79d6ef917a5ae63d90b5454e74ce83256457
alright21 <alberto.giust@studenti.unitn.it>
2021-04-30 09:02:15 +0300

    feat(main): add main template

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+
+
+int main() {
+  std::ifstream in("input.txt");
+  std::ofstream out("output.txt");
+
+  int a, b;
+
+  while(in >> a >> b){
+    
+    //process a, b
+  }
+
+
+  return 0;
+}
